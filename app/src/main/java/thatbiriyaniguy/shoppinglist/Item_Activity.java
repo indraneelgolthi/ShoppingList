a34fac78a28d8b05025a8d095191c1088e852602
Neel <neelsmailid@gmail.com>
2018-01-22 00:02:30 -0600

    Added Menu to Add Item Activity

--- app/src/main/java/thatbiriyaniguy/shoppinglist/Item_Activity.java
+++ app/src/main/java/thatbiriyaniguy/shoppinglist/Item_Activity.java
@@ -2,6 +2,8 @@ package thatbiriyaniguy.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Item_Activity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class Item_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_);
+
+        if(getIntent().hasExtra("TITLE")){
+            setTitle(getIntent().getStringExtra("TITLE"));
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_item_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.save_action){
+            //Save new item code will come here
+        }
+        return true;
     }
 }
